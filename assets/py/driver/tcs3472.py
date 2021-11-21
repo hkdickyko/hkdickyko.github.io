974dc4d1a5671f37480c2453ee7f4eb7a4f07304
hkdickyko <hkdickyko@gmail.com>
2021-11-21 11:48:24 +0800

    commit

--- assets/py/driver/tcs3472.py
+++ assets/py/driver/tcs3472.py
@@ -27,45 +27,42 @@ class TCS3472():
         self._i2c = i2c
         self._address = 0x29
         self._led = led
+        self.cycles = 1
+        self._active = False
         self.gain(1)
         self.integration_time(2.4)
-        self._active = False
 
-    def _register8(self, register, value=None):
+    def _8bits(self, register, value=None):
         register |= _COMMAND_BIT
         if value is None:
             return self._i2c.readfrom_mem(self._address, register, 1)[0]
         data = ustruct.pack('<B', value)
         self._i2c.writeto_mem(self._address, register, data)
 
-    def _register16(self, register, value=None):
+    def _16bits(self, register, value=None):
         register |= _COMMAND_BIT
         if value is None:
             data = self._i2c.readfrom_mem(self._address, register, 2)
             return ustruct.unpack('<H', data)[0]
         data = ustruct.pack('<H', value)
         self._i2c.writeto_mem(self._address, register, data)
-        
-    def readStatus(self):
-        return self._register8(REG_STATUS)
-
-    def readRawData(self):
-        c = self._register16(REG_CDATA)
-        r = self._register16(REG_RDATA)
-        g = self._register16(REG_GDATA)
-        b = self._register16(REG_BDATA)
-        data = (r, g, b, c)
-        return self.html_hex(data)
     
     def _valid(self):
-        return bool(self._register8(_REGISTER_STATUS) & 0x01)
+        return bool(self._8bits(_REGISTER_STATUS) & 0x01)
 
+    def _convertRGB(self, data):
+        r, g, b, c = data
+        r = int(255 * r / c) 
+        g = int(255 * g / c)
+        b = int(255 * b / c)
+        return (r, g , b)
+   
     def read(self, raw=False):
         was_active = self.active()
         self.active(True)
         while not self._valid():
             sleep_ms(int(self._integration_time + 0.9))
-        data = tuple(self._register16(register) for register in (
+        data = tuple(self._16bits(register) for register in (
             _REGISTER_RDATA,
             _REGISTER_GDATA,
             _REGISTER_BDATA,
@@ -74,7 +71,7 @@ class TCS3472():
         self.active(was_active)
         if raw:
             return data
-        return self.convertRGB(data)    
+        return self._convertRGB(data)    
     
     def active(self, value=None):
         if value is None:
@@ -83,17 +80,17 @@ class TCS3472():
         if self._active == value:
             return
         self._active = value
-        enable = self._register8(_REGISTER_ENABLE)
+        enable = self._8bits(_REGISTER_ENABLE)
         if value:
-            self._register8(_REGISTER_ENABLE, enable | _ENABLE_PON)
+            self._8bits(_REGISTER_ENABLE, enable | _ENABLE_PON)
             sleep_ms(3)
-            self._register8(_REGISTER_ENABLE,
+            self._8bits(_REGISTER_ENABLE,
                 enable | _ENABLE_PON | _ENABLE_AEN)
         else:
-            self._register8(_REGISTER_ENABLE,
+            self._8bits(_REGISTER_ENABLE,
                 enable & ~(_ENABLE_PON | _ENABLE_AEN))    
     
-    def _temperature_and_lux(self, data):
+    def cct(self, data):
         r, g, b, c = data
         # 基於 6500K 熒光、3000K 熒光 和 60W 白熾燈值範圍很廣。 
         # RGB 值映射到它們的 XYZ 對應值、Y = 照度或勒克斯 
@@ -113,34 +110,50 @@ class TCS3472():
         if value is None:
             return self._integration_time
         value = min(614.4, max(2.4, value))
-        cycles = int(value / 2.4)
-        self._integration_time = cycles * 2.4
-        return self._register8(_REGISTER_ATIME, 256 - cycles)
+        self.cycles = int(value / 2.4)
+        self._integration_time = self.cycles * 2.4
+        return self._8bits(_REGISTER_ATIME, 256 - self.cycles)
 
     def gain(self, value):
         if value is None:
-            return _GAINS[self._register8(_REGISTER_CONTROL)]
+            return _GAINS[self._8bits(_REGISTER_CONTROL)]
         if value not in _GAINS:
             raise ValueError("gain must be 1, 4, 16 or 60")
-        return self._register8(_REGISTER_CONTROL, _GAINS.index(value))    
+        return self._8bits(_REGISTER_CONTROL, _GAINS.index(value))    
     
-    def set_leds(self, state=0):
-        self._led.value(state)
+    def onLeds(self, flag):
+        if flag :
+            self._led.value(1)
+        else:
+            self._led.value(0)
         
-    def convertRGB(self, data):
-        r, g, b, c = data
-        r = 256 * r / c 
-        g = 256 * g / c
-        b = 256 * b / c
-        return (r, g , b)
-        
-    def html_rgb(self, data):
-        r, g, b, c = data
-        red = pow((int((r/c) * 256) / 255), 2.5) * 255
-        green = pow((int((g/c) * 256) / 255), 2.5) * 255
-        blue = pow((int((b/c) * 256) / 255), 2.5) * 255
-        return red, green, blue
+    def toHex(self, data):
+        r, g, b = data
+        return "{:02x}{:02x}{:02x}".format(int(r), int(g), int(b))
     
+    def threshold(self, cycles=None, min_value=None, max_value=None):
+        if cycles is None and min_value is None and max_value is None:
+            min_value = self._16bits(_REGISTER_AILT)
+            max_value = self._16bits(_REGISTER_AILT)
+            if self._8bits(_REGISTER_ENABLE) & _ENABLE_AIEN:
+                cycles = _CYCLES[self._8bits(_REGISTER_APERS) & 0x0f]
+            else:
+                cycles = -1
+            return cycles, min_value, max_value
+        if min_value is not None:
+            self._16bits(_REGISTER_AILT, min_value)
+        if max_value is not None:
+            self._16bits(_REGISTER_AIHT, max_value)
+        if cycles is not None:
+            enable = self._8bits(_REGISTER_ENABLE)
+            if cycles == -1:
+                self._8bits(_REGISTER_ENABLE, enable & ~(_ENABLE_AIEN))
+            else:
+                self._8bits(_REGISTER_ENABLE, enable | _ENABLE_AIEN)
+                if cycles not in _CYCLES:
+                    raise ValueError("invalid persistence cycles")
+                self._8bits(_REGISTER_APERS, _CYCLES.index(cycles))
+                
     def gammaTable(self):
         self._gamma = array.array('B')
         for i in range(256):
@@ -150,35 +163,4 @@ class TCS3472():
             
     def gamma(self, value):
         return self._gamma[int(value)]
-
-    def html_hex(self, data):
-        r, g, b = self.html_rgb(data)
-        return "{:02x}{:02x}{:02x}".format(int(r), int(g), int(b))
-    
-    def toHex(self, data):
-        r, g, b = data
-        return "{:02x}{:02x}{:02x}".format(int(r), int(g), int(b))
-    
-    def threshold(self, cycles=None, min_value=None, max_value=None):
-        if cycles is None and min_value is None and max_value is None:
-            min_value = self._register16(_REGISTER_AILT)
-            max_value = self._register16(_REGISTER_AILT)
-            if self._register8(_REGISTER_ENABLE) & _ENABLE_AIEN:
-                cycles = _CYCLES[self._register8(_REGISTER_APERS) & 0x0f]
-            else:
-                cycles = -1
-            return cycles, min_value, max_value
-        if min_value is not None:
-            self._register16(_REGISTER_AILT, min_value)
-        if max_value is not None:
-            self._register16(_REGISTER_AIHT, max_value)
-        if cycles is not None:
-            enable = self._register8(_REGISTER_ENABLE)
-            if cycles == -1:
-                self._register8(_REGISTER_ENABLE, enable & ~(_ENABLE_AIEN))
-            else:
-                self._register8(_REGISTER_ENABLE, enable | _ENABLE_AIEN)
-                if cycles not in _CYCLES:
-                    raise ValueError("invalid persistence cycles")
-                self._register8(_REGISTER_APERS, _CYCLES.index(cycles))
-                
+                   
