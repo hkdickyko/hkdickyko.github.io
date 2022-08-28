e07050fea596a0b40d76d9e1db699c3bedf2cb0b
hkdickyko <hkdickyko@gmail.com>
2022-08-28 13:02:00 +0800

    commit

---
+++ assets/js/rc.js
@@ -0,0 +1,13 @@
+function multiplyBy()
+{
+  num1 = document.getElementById("Resistance").value;
+  num2 = document.getElementById("Capacitance").value;
+  document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy()
+{
+  num1 = document.getElementById("Resistance").value;
+  num2 = document.getElementById("Capacitance").value;
+  document.getElementById("result").innerHTML = num1 / num2;
+}
