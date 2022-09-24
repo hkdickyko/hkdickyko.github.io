2de1c725a4f7d8d6d10a25b75eb71da8048e6d55
hkdickyko <hkdickyko@gmail.com>
2022-09-24 14:52:40 +0800

    commit

--- assets/html/water/fragment.glsl
+++ assets/html/water/fragment.glsl
@@ -1,5 +1,4 @@
-uniform sampler2D Matcap;
-    varying vec2 Point;
+uniform sampler2D Matcap;      varying vec2 Point;
     void main(void){
     // texture2D() 获取颜色值
     vec4 color = texture2D(Matcap, Point);
